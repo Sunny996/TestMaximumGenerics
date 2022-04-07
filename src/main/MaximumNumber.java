88b85781c125fe1bebc4b4333973e83ac55d8523
Sunny996 <satlaakash@gmail.com>
2022-04-07 23:39:56 +0530

    ADDED: A method to find maximum of 3 for generic type

--- src/main/MaximumNumber.java
+++ src/main/MaximumNumber.java
@@ -3,11 +3,13 @@ package main;
 public class MaximumNumber {
     public static void main(String[] args) {
         MaximumNumber maximumNumber = new MaximumNumber();
+        System.out.println("The maximum value is " + maximumNumber.maximumTest(5, 10, 20));
+        System.out.println("The maximum value is " + maximumNumber.maximumTest(5f, 10f, 20f));
         System.out.println("The maximum value is " + maximumNumber.maximumTest("Apple", "Banana", "Peach"));
     }
 
-    public String maximumTest(String x, String y, String z) {
-        String max = x;
+    public <E extends Comparable>  E maximumTest(E x, E y, E z) {
+        E max = x;
         if (y.compareTo(max) > 0)
             max = y;
         if (z.compareTo(max) > 0)
--- src/main/TestDemo.java
+++ src/main/TestDemo.java
@@ -9,16 +9,23 @@ public class TestDemo {
 
     @Test
     public void testCase1() {
+        assertEquals(Integer.valueOf(10), maximumNumber.maximumTest(10, 5, 1));
+        assertEquals(Float.valueOf(10), maximumNumber.maximumTest(10f, 5f, 1f));
         assertEquals("Peach", maximumNumber.maximumTest("Peach", "Banana", "Apple"));
+
     }
 
     @Test
     public void testCase2() {
+        assertEquals(Integer.valueOf(10), maximumNumber.maximumTest(5, 10, 1));
+        assertEquals(Float.valueOf(10), maximumNumber.maximumTest(5f, 10f, 1f));
         assertEquals("Peach", maximumNumber.maximumTest("Apple", "Peach", "Banana"));
     }
 
     @Test
     public void testCase3() {
+        assertEquals(Integer.valueOf(10), maximumNumber.maximumTest(1, 5, 10));
+        assertEquals(Float.valueOf(10), maximumNumber.maximumTest(1f, 5f, 10f));
         assertEquals("Peach", maximumNumber.maximumTest("Apple", "Banana", "Peach"));
     }
 }
