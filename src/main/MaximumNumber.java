e1d0296876a0969c8895d39b6ab12a885e978e37
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-07 23:05:54 +0530

    Update MaximumNumber.java

--- src/main/MaximumNumber.java
+++ src/main/MaximumNumber.java
@@ -3,14 +3,15 @@ package main;
 public class MaximumNumber {
     public static void main(String[] args) {
         MaximumNumber maximumNumber = new MaximumNumber();
-        System.out.println("The maximum value is "+maximumNumber.maximumTest(1,2,3));
+        System.out.println("The maximum value is " + maximumNumber.maximumTest(1, 2, 3));
     }
-    public Integer maximumTest(Integer x, Integer y, Integer z){
-        Integer max=x;
-        if(y.compareTo(max)>0)
-            max=y;
-        if(z.compareTo(max)>0)
-            max=z;
+
+    public Integer maximumTest(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
         return max;
     }
 }
