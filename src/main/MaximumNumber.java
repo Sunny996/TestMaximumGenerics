b50619f7a23419f1be26611c73671cc973253811
Sunny996 <satlaakash@gmail.com>
2022-04-10 18:36:28 +0530

    ADDED: A method to find max using sort method

--- src/main/MaximumNumber.java
+++ src/main/MaximumNumber.java
@@ -1,34 +1,38 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MaximumNumber<E extends Comparable> {
     public static void main(String[] args) {
-        MaximumNumber maximumNumberInt = new MaximumNumber(5, 10, 20);
-        MaximumNumber maximumNumberFloat = new MaximumNumber(5f, 10f, 20f);
-        MaximumNumber maximumNumberString = new MaximumNumber("Apple", "Banana", "Peach");
+        MaximumNumber maximumNumberInt = new MaximumNumber(5, 10, 20,30);
+        MaximumNumber maximumNumberFloat = new MaximumNumber(5f, 10f, 20f,30f);
+        MaximumNumber maximumNumberString = new MaximumNumber("Apple", "Banana", "Peach","Orange");
 
         System.out.println("The maximum value is " + maximumNumberInt.maximumTest());
         System.out.println("The maximum value is " + maximumNumberFloat.maximumTest());
         System.out.println("The maximum value is " + maximumNumberString.maximumTest());
     }
 
-    E x, y, z;
+    E x, y, z, t;
 
-    public MaximumNumber(E x, E y, E z) {
+    public MaximumNumber(E x, E y, E z, E t) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.t = t;
     }
 
     public E maximumTest() {
-        return maximumTest(this.x, this.y, this.z);
+        return maximumTest(this.x, this.y, this.z, this.t);
     }
 
-    public static <E extends Comparable> E maximumTest(E x, E y, E z) {
-        E max = x;
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
+    public static <E extends Comparable> E maximumTest(E x, E y, E z, E t) {
+        List<E> list=new ArrayList<E>(Arrays.asList(x,y,z,t));
+        Collections.sort(list,Collections.reverseOrder());
+        E max = list.get(0);
         return max;
     }
 }
--- src/main/TestDemo.java
+++ src/main/TestDemo.java
@@ -5,17 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestDemo {
-    MaximumNumber maximumNumberInt1 = new MaximumNumber(10, 5, 1);
-    MaximumNumber maximumNumberFloat1 = new MaximumNumber(10f, 5f, 1f);
-    MaximumNumber maximumNumberString1 = new MaximumNumber("Peach", "Banana", "Apple");
+    MaximumNumber maximumNumberInt1 = new MaximumNumber(10, 5, 1,3);
+    MaximumNumber maximumNumberFloat1 = new MaximumNumber(10f, 5f, 1f,3f);
+    MaximumNumber maximumNumberString1 = new MaximumNumber("Peach", "Banana", "Apple","Orange");
 
-    MaximumNumber maximumNumberInt2 = new MaximumNumber(5, 10, 1);
-    MaximumNumber maximumNumberFloat2 = new MaximumNumber(5f, 10f, 1f);
-    MaximumNumber maximumNumberString2 = new MaximumNumber("Apple", "Peach", "Banana");
+    MaximumNumber maximumNumberInt2 = new MaximumNumber(5, 10, 1,3);
+    MaximumNumber maximumNumberFloat2 = new MaximumNumber(5f, 10f, 1f,3f);
+    MaximumNumber maximumNumberString2 = new MaximumNumber("Apple", "Peach", "Banana","Orange");
 
-    MaximumNumber maximumNumberInt3 = new MaximumNumber(1, 5, 10);
-    MaximumNumber maximumNumberFloat3 = new MaximumNumber(1f, 5f, 10f);
-    MaximumNumber maximumNumberString3 = new MaximumNumber("Apple", "Banana", "Peach");
+    MaximumNumber maximumNumberInt3 = new MaximumNumber(1, 5, 10,3);
+    MaximumNumber maximumNumberFloat3 = new MaximumNumber(1f, 5f, 10f,3f);
+    MaximumNumber maximumNumberString3 = new MaximumNumber("Apple", "Banana", "Peach","Orange");
 
 
 
